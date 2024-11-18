1a66b1e94ae2e9d5aaa8fe1485e30058283f752b
Gowthamipriya05 <gowthamipriya2004@gmail.com>
2024-11-19 00:42:45 +0530

    update

--- server.js
+++ server.js
@@ -14,19 +14,7 @@ const upload = multer({ storage: multer.memoryStorage() }); // Store file in mem
 const app = express();
 app.use(express.json());
 
-const allowedOrigins = ['https://sportspot-frontend.vercel.app', 'http://localhost:3000'];
-
-app.use(cors({
-  origin: function (origin, callback) {
-    if (allowedOrigins.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true // If cookies/auth headers are needed
-}));
+app.use(cors());
 
 
 
